b612e112912164b5e8b6001d1b313c6bce1ec043
Severus7 <joshuasapz@gmail.com>
2022-06-18 15:29:40 +0800

    dynamic routing

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -1,7 +1,13 @@
+import { useRouter } from "next/router";
+
 const CoffeeStore = () => {
+  const router = useRouter();
+  console.log(router);
+
   return (
     <div>
       <h1>Coffee Store Page</h1>
+      <p>{router.query.id}</p>
     </div>
   );
 };
