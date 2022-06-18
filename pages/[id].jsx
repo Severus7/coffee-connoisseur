c8de8bed535105e1e366b7d71f76c228ed319000
Severus7 <joshuasapz@gmail.com>
2022-06-18 19:08:17 +0800

    done chapter 2

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,13 +1,17 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const DynamicRoute = () => {
   const router = useRouter();
-  const query = router.query.dynamic;
+  const query = router.query.id;
 
   console.log("query", query);
 
   return (
     <div>
+      <Head>
+        <title>{query}</title>
+      </Head>
       <h1>Welcome to Next.js with Ankita</h1>
       <p>{query}</p>
     </div>
