0fda73cb5a1c097030a1307f67df6235f247b5b0
Severus7 <joshuasapz@gmail.com>
2022-06-18 18:20:25 +0800

    added route using Link component

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStore = () => {
   const router = useRouter();
@@ -6,6 +7,9 @@ const CoffeeStore = () => {
 
   return (
     <div>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
       <h1>Coffee Store Page</h1>
       <p>{router.query.id}</p>
     </div>
