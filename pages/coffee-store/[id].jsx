1a9e22ed0304aa407244bad175fd0b8de3f87541
Severus7 <joshuasapz@gmail.com>
2022-06-18 18:54:53 +0800

    added dynamic routing using Link component

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -12,6 +12,10 @@ const CoffeeStore = () => {
       </Link>
       <h1>Coffee Store Page</h1>
       <p>{router.query.id}</p>
+      <br />
+      <Link href="/coffee-store/dynamic">
+        <a>Go to dynamic page</a>
+      </Link>
     </div>
   );
 };
