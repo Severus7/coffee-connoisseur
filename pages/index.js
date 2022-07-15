b3f98021104526d261920b2b2f74bce364280f58
Severus7 <joshuasapz@gmail.com>
2022-07-15 14:09:11 +0800

    card component structure

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Banner from "../components/banner";
+import Card from "../components/card";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
     console.log("Banner Button");
-    // handleTrackLocation();
   };
 
   return (
@@ -26,6 +26,11 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+        <Card
+          name="Darkhorse Coffee"
+          href="/coffee-store/darkhorse-coffee"
+          imgUrl="/static/hero-image.png"
+        />
       </main>
     </div>
   );
